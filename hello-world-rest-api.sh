0b0b2c630028a51d934b0414ebfb945eed524af4
l.gutierrez <l.gutierrez@students.salle.url.edu>
2023-01-30 20:56:26 +0100

    Creado script de ejecucion del programa

---
+++ hello-world-rest-api.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+# Package the program
+mvn package
+
+#Build docker image
+docker build -t springio/gs-spring-boot-docker .
+
+#Run program
+docker run -p 8080:8080 springio/gs-spring-boot-docker
