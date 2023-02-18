f76dbca87e57c0f8b95e4ea66ab9fa5d3709eb90
l.gutierrez <l.gutierrez@students.salle.url.edu>
2023-02-18 18:48:07 +0100

    Separated integration test to another class.

--- src/test/java/com/example/springbootdocker/SpringBootDockerApplicationTests.java
+++ src/test/java/com/example/springbootdocker/SpringBootDockerApplicationTests.java
@@ -1,25 +1,15 @@
 package com.example.springbootdocker;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
 class SpringBootDockerApplicationTests {
 	@Autowired
 	private SpringBootDockerApplication controller;
@@ -27,8 +17,6 @@ class SpringBootDockerApplicationTests {
 	private int port;
 	@Autowired
 	private TestRestTemplate restTemplate;
-	@Autowired
-	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() throws Exception{
@@ -38,11 +26,6 @@ class SpringBootDockerApplicationTests {
 	public void greetingShouldReturnDefaultMessage() throws Exception {
 		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
 				String.class)).contains("Hello Docker World");
-	}
-	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
-		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Hello Docker World")));
 	}
 
 }
