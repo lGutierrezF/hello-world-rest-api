3a0b23bd7c1042a8ed9d520824b43db86a5d04f4
lGutierrezF <93689570+lGutierrezF@users.noreply.github.com>
2023-02-21 14:18:15 +0100

    Feature (#11)
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.

--- src/test/java/com/example/springbootdocker/SpringBootDockerApplicationIT.java
+++ src/test/java/com/example/springbootdocker/SpringBootDockerApplicationIT.java
@@ -3,6 +3,8 @@ package com.example.springbootdocker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -12,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
+@SpringBootTest
 public class SpringBootDockerApplicationIT {
     @Autowired
     private MockMvc mockMvc;
