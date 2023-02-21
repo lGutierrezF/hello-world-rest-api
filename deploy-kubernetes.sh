896352eda6a21c2fb560c14093516b4e142c7f7c
lGutierrezF <93689570+lGutierrezF@users.noreply.github.com>
2023-02-21 20:55:25 +0100

    Feature (#23)
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Removed commented lines.
    
    * Added kubernetes deployment script
    
    * Feature (#13)
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Update commit-stage.yml
    
    * Update commit-stage.yml
    
    * Update commit-stage.yml
    
    * Feature (#16)
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Update merge-to-main.yml
    
    * Feature (#18)
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Removed commented lines.
    
    * Update merge-to-main.yml
    
    * Improved wording in merge-to-main script.
    
    * Improved wording in merge-to-main script.
    
    * Added kubernetes pod deployment yaml
    
    * Fixed kubernetes deployment script

--- deploy-kubernetes.sh
+++ deploy-kubernetes.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 # Delete previous pods
-kubectl delete all --all
+kubectl delete pods --all
+kubectl delete services --all
 # Apply configuration
 kubectl apply -f pod-hello-world-rest-api-7684687d7b-rkqn4.yaml
 kubectl apply -f service-hello-world-rest-api-service.yaml
